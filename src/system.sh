2309a9ccb32b6aef6c693af1dc7e3dafca4ee006
ShenMian <sms_school@outlook.com>
2022-08-14 00:00:15 +0800

    feat: add some functions

--- src/system.sh
+++ src/system.sh
@@ -13,3 +13,36 @@ function require {
         exit 1
     fi
 }
+
+function get_host_name {
+    uname -n
+}
+
+function get_system_arch {
+    case $(uname --m) in
+    x86_64)
+        echo 'x64'
+        ;;
+    i*86)
+        echo 'x86'
+        ;;
+    *)
+        ;;
+    esac
+}
+
+function get_cpu_name {
+    lscpu | grep "Model name" | cut -d ":" -f 2 | xargs
+}
+
+function get_cpu_num {
+    nproc --all
+}
+
+# $1: USB 端口号
+function get_usb_ver {
+    lsusb | grep "root hub" | cut -d ' ' -f 2,9 | grep "$1" | cut -d ' ' -f 2
+}
+function get_usb_device_num {
+    echo $(($(lsusb | cut -d ' ' -f 2 | grep "$1" | wc -l) - 1))
+}
