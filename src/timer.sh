9c5c07860593a390a10f8af05590982734bd0e88
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-08-13 17:40:03 +0000

    feat: update timer

--- src/timer.sh
+++ src/timer.sh
@@ -3,20 +3,25 @@
 import oo
 
 class Timer
-    fn Timer
-    fn start
-    fn print
+    pub
+        fn Timer
+        fn restart
+        fn getSeconds
+        fn getMilliseconds
     var start_time
 
 function Timer::Timer {
-    echo hi
+    start_time="$(date +%s.%2N)"
 }
 
-function Timer::start {
-    start_time=$(date +%s.%2N)
+function Timer::restart {
+    start_time="$(date +%s.%2N)"
 }
 
-function Timer::print {
-    local elapse=$(echo "$(date +%s.%2N) - $start_time" | bc)
-    echo $elapse
+function Timer::getSeconds {
+    echo $( echo "$(date +%s.%2N) - $start_time" | bc | awk '{printf "%.0f", $0}' )
+}
+
+function Timer::getMilliseconds {
+    echo $( echo "$(date +%s.%2N) - $start_time" | bc | awk '{printf "%.2f", $0}' )
 }
