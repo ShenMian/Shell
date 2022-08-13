bed70fa043ef6dbee64b68a10e3a9ebbf8db45dc
ShenMian <sms_school@outlook.com>
2022-08-13 18:02:21 +0800

    refactor: rename some functions

--- src/io.sh
+++ src/io.sh
@@ -3,27 +3,23 @@
 source "$(dirname $0)/require.sh"
 require color
 
-echo_with_color() {
+echo_colored() {
   printf '%b\n' "$1$2$Color_off" >&2
 }
 
-println() {
-  printf '%b\n' "$1$2$Color_off" >&2
-}
-
-function success() {
-  println "${Green}[✔]${Color_off} ${1}${2}"
+function good() {
+  echo "${Green}[✔]${Color_off} ${1}${2}"
 }
 
 function info() {
-  println "${Blue}[➭]${Color_off} ${1}${2}"
+  echo "${Blue}[➭]${Color_off} ${1}${2}"
 }
 
 function error() {
-  println "${Red}[✘]${Color_off} ${1}${2}"
+  echo "${Red}[✘]${Color_off} ${1}${2}"
   exit 1
 }
 
 function warn() {
-  println "${Yellow}[⚠]${Color_off} ${1}${2}"
+  echo "${Yellow}[⚠]${Color_off} ${1}${2}"
 }
